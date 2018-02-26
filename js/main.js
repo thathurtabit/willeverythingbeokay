933b98c12de2643e47730c8b07071c5ca9e957e0
Stephen Fairbanks <biggste2000@hotmail.com>
2018-02-26 20:02:47 +0000

    Feat: Adding that JS flava

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const title = "Will everything be ok?"
+const titleArray = [...title]
+const titleElement = document.getElementsByTagName('h1')[0]
+
+titleArray.map((letter, index) => {
+   return titleElement.insertAdjacentHTML('beforeend', `<span>${letter}</span`)
+})
+
+let newTitleArray = titleElement.getElementsByTagName('span')
+let animateArray = [...newTitleArray]
+
+animateArray.map((letter, index) => {
+    letter.animate(
+        [
+            { transform: 'translateY(0)', opacity: 0.5 },
+            { transform: 'translateY(-30px)', opacity: 1 },
+            { transform: 'translateY(0)', opacity: 0.5 },
+        ], {
+            duration: 3000,
+            iterations: Infinity
+        }
+    )
+})
